3de6abc48ebefb3b4a4653ef23668d23cafdf56e
Ashok Kalidoss <ashok@Ashoks-Air.lan>
2019-03-11 22:27:42 +1100

    commit first version

---
+++ src/main/java/com/practice/practiceSpringBootUpgraded/PracticeSpringBootUpgradedApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practiceSpringBootUpgraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootUpgradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootUpgradedApplication.class, args);
+	}
+
+}
